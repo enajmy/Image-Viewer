bc6e53f2199540744aea0e8665749324e72829e3
enajmy <ethannajmy1@gmail.com>
2022-11-12 17:39:03 -0800

    Create main.c
    Source code.

---
+++ main.c
@@ -0,0 +1,17 @@
+/********************************************************************
+* File: main.c
+*
+* Description: main file for sobel filter algorithm
+*
+* Author: Ethan Najmy
+*
+* Revision History:
+*	- 1.0: file created
+*
+********************************************************************/
+#include "imgViewer.h"
+
+int main(void){
+	viewFile();
+	return 0;
+}

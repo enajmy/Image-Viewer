4663e56e45acb973fa8cc350d6479b38b32ffc07
enajmy <ethannajmy1@gmail.com>
2022-11-12 17:38:58 -0800

    Create imgViewer.h
    Source code.

---
+++ imgViewer.h
@@ -0,0 +1,18 @@
+/********************************************************************
+* File: imgViewer.h
+*
+* Description: header file for imgViewer.c (cmd-line image viewer)
+*
+* Author: Ethan Najmy
+*
+* Revision History:
+*	- 1.0: file created
+*
+********************************************************************/
+#ifndef SRC_IMGVIEWER_H
+#define SRC_IMGVIEWER_H
+
+// Function Declaration
+int viewFile(void);
+
+#endif
